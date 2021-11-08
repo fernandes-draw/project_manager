d6d7e882b092d02be1ef4230a3c1679bc5c743e9
Everson Fernandes <fernandes_draw@hotmail.com>
2021-11-08 17:00:00 -0300

    Criação das views "Produto" e "Código"

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=150)
+    data_cadastro = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome + " - " + str(self.data_cadastro)
+
+class Codigo(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.codigo + " - " + str(Produto.objects.get())
+    

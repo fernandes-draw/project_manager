fda9a44e137e4511e85a31446e9af4255fb82f18
Everson Fernandes <fernandes_draw@hotmail.com>
2021-11-10 08:14:59 -0300

    Listagem de produtos na home

--- produto/models.py
+++ produto/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Produto(models.Model):
     nome = models.CharField(max_length=150)
-    data_cadastro = models.DateField(auto_now_add=True)
+    data_cadastro = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.nome + " - " + str(self.data_cadastro)
--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Produto
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'produto/index.html')
+    produtos = Produto.objects.all()
+    return render(request, 'produto/index.html', {'produtos': produtos})
